b6d984d2f65b3cf0b76a720415e39aff34d4d601
albabernal03 <ALBABERO@MYUAX.COM>
2022-04-20 16:31:08 +0200

    algo que he pensadp

--- ataque.py
+++ ataque.py
@@ -1,4 +1,20 @@
 from pokemon import Pokemon
+import random 
 
-def pokemon_maquina
+pokemon_jugador = Pokemon.pokemon_jugador()
+pokemon_maquina = Pokemon.pokemon_maquina()
+
+# definir funcion ataque
+def ataque(pokemon_jugador, pokemon_maquina):
+    if pokemon_jugador.arma == 'puñetazo':
+        pokemon_maquina.salud == pokemon_maquina.salud - 5
+    elif pokemon_jugador.arma == 'patada':
+        pokemon_maquina.salud == pokemon_maquina.salud - 3
+    elif pokemon_jugador.arma == 'codazo':
+        pokemon_maquina.salud == pokemon_maquina.salud - 3
+    elif pokemon_jugador.arma == 'cabezazo':
+        pokemon_maquina.salud == pokemon_maquina.salud - 4
+    else:
+        print('No se ha podido realizar el ataque')
+        
 
